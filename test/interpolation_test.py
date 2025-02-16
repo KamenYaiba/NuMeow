ce520b2a417ad2b0ba5a5f9d684b4c65ad28b783
ahmadammarjr <110498902+KamenYaiba@users.noreply.github.com>
2025-02-16 16:12:38 +0300

    Add testcases for nevilles_method()

--- test/interpolation_test.py
+++ test/interpolation_test.py
@@ -1,12 +1,15 @@
 from src.interpolation import nevilles_method
 
-n_tests = 1
 tests = []
 
 test = nevilles_method([8.1, 8.3, 8.6], [16.94410, 17.56492, 18.50515], 8.4) == 17.87713
 tests.append(test)
+test = nevilles_method([8.1, 8.3], [16.94410, 17.56492, 18.50515], 8.4) is False
+tests.append(test)
+test = nevilles_method([8.1, 8.3, 8.6], [], 8.4) is False
+tests.append(test)
 
-for i in range(n_tests):
+for i in range(len(tests)):
     if tests[i]:
         print(f'Test {i+1}: Passed ✔')
     else:
