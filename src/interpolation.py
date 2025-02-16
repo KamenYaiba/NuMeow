c646c7197bb84c3d8681b03938aa75fab7f510a3
ahmadammarjr <110498902+KamenYaiba@users.noreply.github.com>
2025-02-16 22:14:19 +0300

    Implement nevilles_method_steps()

--- src/interpolation.py
+++ src/interpolation.py
@@ -13,10 +13,31 @@ def nevilles_method(x, y, xS):
     return y[0]
 
 
+#NOT TESTED
+#TODO: write testcases
+def nevilles_method_steps(x, y, xS): #uses more space/might be slower?
+    if len(x) != len(y):
+        return False
+
+    columns = len(x) -1
+    diff = 1
+    result = y[:]
+    result_pointer = columns
+
+    for i in range(columns):
+        k = columns - i
+        for j in range(k):
+            print(f'{i}  {j}  {k}  {result_pointer}  {len(result)}  {result_pointer - k + 1}')
+            result.append((result[result_pointer - k + 1] * (xS - x[j]) - result[result_pointer - k] * (xS - x[j+diff])) / (x[j+diff] - x[j]))
+            result_pointer += 1
+        diff += 1
+
+    return result
+
+
 #TODO: lagrange_polynomial()
 #TODO: divided_differences()
 #TODO: hermites_method()
-#TODO: nevilles_method_steps()
 #TODO: lagrange_polynomial_steps()
 #TODO: divided_differences_steps()
 #TODO: hermites_method_steps()
