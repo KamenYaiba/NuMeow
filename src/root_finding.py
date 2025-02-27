cc099853790cf5ebc8dc007c2ee7d21df7cd6fda
ahmadammarjr <110498902+KamenYaiba@users.noreply.github.com>
2025-02-27 06:35:25 +0300

    Create root_finding.py

---
+++ src/root_finding.py
@@ -0,0 +1,12 @@
+
+
+
+#TODO: newtons_method()
+#TODO: secant_method()
+#TODO: fixed_point_iteration()
+#TODO: bisection_method()
+#TODO: newtons_method_steps()
+#TODO: secant_method_steps()
+#TODO: fixed_point_iteration_steps()
+#TODO: bisection_method_steps()
+
