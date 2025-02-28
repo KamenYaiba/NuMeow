25fe80eeb5b4b5a1eef8b21e152cce1bb138d0b7
ahmadammarjr <110498902+KamenYaiba@users.noreply.github.com>
2025-02-28 13:57:35 +0300

    implement elementary_symmetric_sum()

--- src/helping_functions.py
+++ src/helping_functions.py
@@ -2,36 +2,28 @@ import itertools
 import math
 
 
-#NOT DONE
 def elementary_symmetric_sum(input_list):
     degree = len(input_list)
-    result_list = [1, sum(input_list)]
-
-    temp_sum = 0
-    iterations = degree - 2 #a list of length n prodeces n+1 constants, and 3 of these constants do not
-                            #need iterations to calculate, therfore: n+1 - 3 = n-1
-    for itr in range(iterations):
-        for i in range(degree):
-            for j in range(i+1, degree):
-                temp_sum = temp_sum + (input_list[i] * input_list[j])
-
-        result_list.append(temp_sum)
-
-    result_list.append(math.prod(input_list))
+    result_list = []
+    for i in range(degree+1):
+        result_list.append(sum_product_length_n_combinations(input_list, i))
     return result_list
 
 
 #let array = [1, 2, 3, 4]
-#product_length_n_groups(array, 1) should return 1 + 2 + 3 + 4
-#product_length_n_groups(array, 2) should return 1x2 + 1x3 + 1x4 + 2x3 + 2x4 + 3x4
-#product_length_n_groups(array, 3) should return 1x2x3 + 1x2x4 + 1x3x4 +2x3x4
-#product_length_n_groups(array, 4) should return 1x2x3x4
+#product_length_n_combinations(array, 1) should return 1 + 2 + 3 + 4
+#product_length_n_combinations(array, 2) should return 1x2 + 1x3 + 1x4 + 2x3 + 2x4 + 3x4
+#product_length_n_combinations(array, 3) should return 1x2x3 + 1x2x4 + 1x3x4 +2x3x4
+#product_length_n_combinations(array, 4) should return 1x2x3x4
 def sum_product_length_n_combinations(array, n):
+    if n == 0:
+        return 1
     if n == 1:
         return sum(array)
     if len(array) == n:
         return math.prod(array)
 
+    #no im not using the one-liner "pythonic way"
     combinations = itertools.combinations(array, n)
     res_sum = 0
     for c in combinations:
@@ -39,5 +31,3 @@ def sum_product_length_n_combinations(array, n):
 
     return res_sum
 
-
-print(sum_product_length_n_combinations([1,2,3], 2))
